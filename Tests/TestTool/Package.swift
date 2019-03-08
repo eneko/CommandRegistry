19fa8584d3dc500829592948de98f75e0716eb22
Eneko Alonso <eneko.alonso@gmail.com>
2019-03-08 10:07:04 -0800

    Add tag for TestTool

--- Tests/TestTool/Package.swift
+++ Tests/TestTool/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TestTool",
     dependencies: [
-        .package(url: "../../", .branch("feature/upgrade-command")),
+        .package(url: "../../", from: "0.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
