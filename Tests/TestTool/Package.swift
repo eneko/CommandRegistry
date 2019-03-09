a812ee5913a552a6a74f2523dce369bb48eab266
Eneko Alonso <eneko.alonso@gmail.com>
2019-03-08 18:30:06 -0800

    Another try

--- Tests/TestTool/Package.swift
+++ Tests/TestTool/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TestTool",
     dependencies: [
-        .package(url: "../../", from: "0.2.0"),
+        .package(url: "https://github.com/eneko/CommandRegistry", from: "0.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
